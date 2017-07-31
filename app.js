f0491c01e702a0bea604d0a1b3777ecb9a56ffbb
DeliaCodes <deliacodes@gmail.com>
2017-07-30 17:52:53 -0700

    I think keeping git open leads to merging issues

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 "use strict";
 
-/* on click start to start the quiz 
+/* click to start the quiz - it replaces the div with the correct quiz style
 */
 $('#start').click(function () {
-
+	$('#quiz').empty();
+	$('#quiz').append()
 }
+);
 
-
-	);
-
-$()
+var correctAnswers = 0;
+var	incorrectAnswers = 0;
+//This is the click of the next page (and maybe the other pages thereafter) $().
 
