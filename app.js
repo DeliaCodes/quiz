66b3ca1a2a4f4cac022e09a8e9a77a28be1f3b9d
deliacodes <deliacodes@gmail.com>
2017-08-07 07:44:27 -0700

    It works! Well almost.

--- app.js
+++ app.js
@@ -13,167 +13,133 @@ var	incorrectCount = 0;
 
 $('#start').click(function () {
 	$('#intro').hide();
-	$('#question1').toggle();});
+	$('#question1').toggle();
+
+});
  /*Skeleton form at all time and then just change content. Currently loading form after JS is loaded.
  base form that checks if its right and updates the form
  class to right answer or current answers in a state and checked against that state.
  Container - word. Wrong Answers. Right Answer. And Function to update form based on that data system.
  */
 
-//currently broken - correct answer is yellow  
-$('#question1').submit(function(){
-	event.preventDefault();
-	var userAnswer = $('#question1 :input').val();
-	if ( userAnswer == currentAnswer) {correctCount++}
-	else { incorrectCount++}
-}
-	);
-
-
 
 //Donn question
 $('#continue1').click(function(){
-		$('#question1').detach();
+	var userAnswer = $('#question1 :input').val();
+	if ( userAnswer == currentAnswer) {correctCount++}
+	else { incorrectCount++}
+
+	$('#question1').detach();
 	$('#question2').toggle();
+
 }
 );
 
-$('#question2').submit(function(){
-	event.preventDefault();
+
+//Dearg
+$('#continue2').click(function(){
 	currentAnswer = 'brown';
 	var userAnswer = $('#question2 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Dearg
-$('#continue2').click(function(){
 	$('#question2').detach();
 	$('#question3').toggle();
 }
 );
 
-$('#question3').submit(function(){
-	event.preventDefault();
+
+//Ban
+$('#continue3').click(function(){
 	currentAnswer = 'red';
 	var userAnswer = $('#question3 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Ban
-$('#continue3').click(function(){
 	$('#question3').detach();
 	$('#question4').toggle();
 }
 );
 
-$('#question4').submit(function(){
-	event.preventDefault();
+
+//Glas
+$('#continue4').click(function(){
 	currentAnswer = 'white';
 	var userAnswer = $('#question4 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-//Glas
-$('#continue4').click(function(){
-		$('#question4').detach();
+	$('#question4').detach();
 	$('#question5').toggle();
 }
 );
 
-$('#question5').submit(function(){
-	event.preventDefault();
+
+
+//Bandearg
+$('#continue5').click(function(){
 	currentAnswer = 'green';
 	var userAnswer = $('#question5 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Bandearg
-$('#continue5').click(function(){
 	$('#question5').detach();
 	$('#question6').toggle();
 }
 );
 
-$('#question6').submit(function(){
-	event.preventDefault();
+
+
+//Gorm
+$('#continue6').click(function(){
 	currentAnswer = 'pink';
 	var userAnswer = $('#question6 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Gorm
-$('#continue6').click(function(){
 	$('#question6').detach();
 	$('#question7').toggle();
 }
 );
 
-$('#question7').submit(function(){
-	event.preventDefault();
+
+
+//Corcra
+$('#continue7').click(function(){
 	currentAnswer = 'blue';
 	var userAnswer = $('#question7 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Corcra
-$('#continue7').click(function(){
 	$('#question7').detach();
 	$('#question8').toggle();
 }
 );
 
-$('#question8').submit(function(){
-	event.preventDefault();
+
+//Dubh
+$('#continue8').click(function(){
 	currentAnswer = 'purple';
 	var userAnswer = $('#question8 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//Dubh
-$('#continue8').click(function(){
-		$('#question8').detach();
+	$('#question8').detach();
 	$('#question9').toggle();
 }
 );
 
-$('#question9').submit(function(){
-	event.preventDefault();
-	currentAnswer = 'black';
+
+
+$('#continue9').click(function(){
+		currentAnswer = 'black';
 	var userAnswer = $('#question9 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-$('#continue9').click(function(){
-$('#question9').detach();
+	$('#question9').detach();
 	$('#question10').toggle();
 });
 
-$('#question10').submit(function(){
-	event.preventDefault();
+
+//This is the results page - ref http://www.omniglot.com/language/colours/irish.php
+$('#continue10').click(function(){
 	currentAnswer = 'grey';
 	var userAnswer = $('#question10 :input');
 	if ( userAnswer == currentAnswer) {++correctCount}
 	else { incorrectCount++}
-}
-	);
-
-//This is the results page - ref http://www.omniglot.com/language/colours/irish.php
-$('#continue10').click(function(){
 	$('#question10').detach();
 	$('#correct').html("You got " + correctCount + " colors right!")
 	$('#incorrect').html("You got " + incorrectCount + " colors wrong!")
