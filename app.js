2ed9f4c6f5f6b14a834015d4d1115d455e79cec5
deliacodes <deliacodes@gmail.com>
2017-08-11 12:19:10 -0700

    added answers object, halved questions, added question numbers

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ you can hide the questions and then unhide them to progress which is what I've c
 var currentAnswer = 'yellow';
 var correctCount = 0;
 var	incorrectCount = 0;
+var answers = {
+	bui : 'yellow',
+	donn : 'brown',
+	dearg : 'red',
+	ban : 'white',
+	glas : 'green',
+};
 
 //
 
@@ -26,7 +33,7 @@ $('#start').click(function () {
 //Donn question
 $('#continue1').click(function(){
 	var userAnswer = $('#question1 :input').val();
-	if ( userAnswer == currentAnswer) {correctCount++}
+	if ( userAnswer ==  answers['bui']) {correctCount++}
 	else { incorrectCount++}
 
 	$('#question1').detach();
@@ -38,9 +45,8 @@ $('#continue1').click(function(){
 
 //Dearg
 $('#continue2').click(function(){
-	currentAnswer = 'brown';
 	var userAnswer = $('#question2 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
+	if ( userAnswer == answers['donn']) {++correctCount}
 	else { incorrectCount++}
 	$('#question2').detach();
 	$('#question3').toggle();
@@ -50,9 +56,8 @@ $('#continue2').click(function(){
 
 //Ban
 $('#continue3').click(function(){
-	currentAnswer = 'red';
 	var userAnswer = $('#question3 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
+	if ( userAnswer == answers['dearg']) {++correctCount}
 	else { incorrectCount++}
 	$('#question3').detach();
 	$('#question4').toggle();
@@ -62,9 +67,8 @@ $('#continue3').click(function(){
 
 //Glas
 $('#continue4').click(function(){
-	currentAnswer = 'white';
 	var userAnswer = $('#question4 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
+	if ( userAnswer == answers['ban']) {++correctCount}
 	else { incorrectCount++}
 	$('#question4').detach();
 	$('#question5').toggle();
@@ -72,75 +76,12 @@ $('#continue4').click(function(){
 );
 
 
-
-//Bandearg
+//This is the results page - ref http://www.omniglot.com/language/colours/irish.php
 $('#continue5').click(function(){
-	currentAnswer = 'green';
 	var userAnswer = $('#question5 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
+	if ( userAnswer == answers['glas']) {++correctCount}
 	else { incorrectCount++}
 	$('#question5').detach();
-	$('#question6').toggle();
-}
-);
-
-
-
-//Gorm
-$('#continue6').click(function(){
-	currentAnswer = 'pink';
-	var userAnswer = $('#question6 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
-	else { incorrectCount++}
-	$('#question6').detach();
-	$('#question7').toggle();
-}
-);
-
-
-
-//Corcra
-$('#continue7').click(function(){
-	currentAnswer = 'blue';
-	var userAnswer = $('#question7 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
-	else { incorrectCount++}
-	$('#question7').detach();
-	$('#question8').toggle();
-}
-);
-
-
-//Dubh
-$('#continue8').click(function(){
-	currentAnswer = 'purple';
-	var userAnswer = $('#question8 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
-	else { incorrectCount++}
-	$('#question8').detach();
-	$('#question9').toggle();
-}
-);
-
-
-
-$('#continue9').click(function(){
-		currentAnswer = 'black';
-	var userAnswer = $('#question9 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
-	else { incorrectCount++}
-	$('#question9').detach();
-	$('#question10').toggle();
-});
-
-
-//This is the results page - ref http://www.omniglot.com/language/colours/irish.php
-$('#continue10').click(function(){
-	currentAnswer = 'grey';
-	var userAnswer = $('#question10 :input');
-	if ( userAnswer == currentAnswer) {++correctCount}
-	else { incorrectCount++}
-	$('#question10').detach();
 	$('#correct').html("You got " + correctCount + " colors right!")
 	$('#incorrect').html("You got " + incorrectCount + " colors wrong!")
 	$('#results').toggle();
